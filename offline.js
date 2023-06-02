c5e45668e24ae92de75bfcfa5571733104a8f9ff
WAN LATIFAH HAZIRAH WAN HAIRULDIN <122227369+lmaonadee@users.noreply.github.com>
2023-06-02 15:37:41 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1685691348,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/pipebottom-sheet0.png",
+		"images/pipetop-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"media/swing-whoosh-110410.m4a",
+		"media/swing-whoosh-110410.ogg",
+		"media/taylor swift - you're losing me.m4a",
+		"media/taylor swift - you're losing me.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
